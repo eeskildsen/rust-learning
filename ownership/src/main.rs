3e20c3030ceb0c4dfd5b7a26c7615fc3b35457af
Eric Eskildsen <eeskildsen@gmail.com>
2020-02-10 11:43:15 -0500

    In ownership example, calculate name length

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
     let mut s = String::from("Ricky");
     add_last_name(&mut s);
+    calculate_name_length(&s);
 
     println!("{}", s);
+    println!("It's {} characters long", calculate_name_length(&s));
 }
 
 fn add_last_name(s: &mut String) {
     s.push_str(" Eskildsen");
+}
+
+fn calculate_name_length(s: &String) -> usize {
+    s.len()
 }
